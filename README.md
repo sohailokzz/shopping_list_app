7e05e5fdb26b6e02e096fa41e8fa3ffc677628d2
HP Computer s <sohailokzz@gmail.com>
2022-06-12 11:31:16 +0500

    Shopping List Design

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Initial project has been created and commited already.
 Set up the initial project by remove the built in counter app cod.
 Shuffle the code create view.
 Adding the google fonts package so we can easily use any fonts if required for our app.
+
+Model class as product model has been added with two fields that will be require to create shoping list.
--- lib/views/shopping_list_view.dart
+++ lib/views/shopping_list_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../model/product_model.dart';
+
 class ShoppingList extends StatefulWidget {
   const ShoppingList({Key? key}) : super(key: key);
 
@@ -9,6 +11,7 @@ class ShoppingList extends StatefulWidget {
 }
 
 class _ShoppingListState extends State<ShoppingList> {
+  List<ProductModel> allProducts = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +24,39 @@ class _ShoppingListState extends State<ShoppingList> {
           ),
         ),
         centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: ((context, index) {
+          return Card(
+            elevation: 5.0,
+            child: ListTile(
+              title: Text(
+                'Product Name',
+                style: GoogleFonts.poppins(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+              subtitle: Text(
+                'Quantity',
+                style: GoogleFonts.poppins(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          );
+        }),
       ),
     );
   }
